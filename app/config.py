90dabc0f3537c499649030991212392507ed3a55
Manickandan Govindarajan <manickandan@gmail.comgit config --global user.name piyush>
2026-02-14 17:23:21 -0500

    updated config changes and deployment related chnages

--- app/config.py
+++ app/config.py
@@ -1,14 +1,14 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname: str = "localhost"
-    database_port: str = "5432"
-    database_password: str = "PAssw0rd"
-    database_name: str = "fastapi"
-    database_username: str = "postgres"
-    secret_key: str = "mysecretkey"
-    algorithm: str = "HS256"    
-    access_token_expire_minutes: int = 30
+    database_hostname: str 
+    database_port: str 
+    database_password: str 
+    database_name: str
+    database_username: str
+    secret_key: str 
+    algorithm: str    
+    access_token_expire_minutes: int 
 
     class Config:
         env_file = ".env"
